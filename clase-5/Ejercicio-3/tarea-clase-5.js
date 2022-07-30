3952d8e8856658747dc82812760bf7034450f678
BlackSharkXII <perdomobraian@gmail.com>
2022-07-30 12:37:13 -0300

    Realiza toda la logica para el ejercicio

--- clase-5/Ejercicio-3/tarea-clase-5.js
+++ clase-5/Ejercicio-3/tarea-clase-5.js
@@ -1,7 +1,32 @@
 const $botonCalcular = document.querySelector('#boton-calcular');
 
 $botonCalcular.onclick = function () {
+    /*Las constantes se guardan en un array para luego sumarlas con las funcion devolverValorToltal*/
+    const $horasVideos = [...document.querySelectorAll('.clase-horas')];
+    const $minutosVideos = [...document.querySelectorAll('.clase-minutos')];
+    const $segundosVideos = [...document.querySelectorAll('.clase-segundos')];
+
+    let valorTotalHoras = $horasVideos
+        .map((valor) => Number(valor.value))
+        .reduce((valorPrimero, valorUltimo) => valorPrimero + valorUltimo, 0);
+    let valorTotalMinutos = $minutosVideos
+        .map((valor) => Number(valor.value))
+        .reduce((valorPrimero, valorUltimo) => valorPrimero + valorUltimo, 0);
+    let valorTotalSegundos = $segundosVideos
+        .map((valor) => Number(valor.value))
+        .reduce((valorPrimero, valorUltimo) => valorPrimero + valorUltimo, 0);
+
+    valorTotalMinutos += valorTotalSegundos / 60;
+    valorTotalSegundos = valorTotalSegundos % 60;
+
+    valorTotalHoras += valorTotalMinutos / 60;
+    valorTotalMinutos = valorTotalMinutos % 60;
+
     const $mensajeResultado = document.querySelector('#mensaje-resultado');
-    $mensajeResultado.innerHTML = 'Las clases duran en total: ';
+    $mensajeResultado.innerHTML = `Las clases duran en total: ${Math.trunc(
+        valorTotalHoras
+    )} horas, ${Math.trunc(valorTotalMinutos)} minutos, ${Math.trunc(
+        valorTotalSegundos
+    )} segundos`;
     return false;
 };
