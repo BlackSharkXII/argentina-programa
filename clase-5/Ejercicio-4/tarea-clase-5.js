d5eb5f2c4ee7b7d8759be56db00760611da5dce0
BlackSharkXII <perdomobraian@gmail.com>
2022-08-16 18:59:03 -0300

    Agrega la funciones
    Esto es con el fin de poder reutilizarlos.

--- clase-5/Ejercicio-4/tarea-clase-5.js
+++ clase-5/Ejercicio-4/tarea-clase-5.js
@@ -1,5 +1,63 @@
-// Obtenemos los valores de los li y lo ponemos un array.
+// Obtenemos los valores de los li y lo ponemos en un array.
 
-let arrayLi = [...document.querySelectorAll('li')].map(
-    (element) => element.innerText
-);
+function convertirEnArray(elemento) {
+    const arrayLi = [...document.querySelectorAll(elemento)].map((element) =>
+        Number(element.innerText)
+    );
+    return arrayLi;
+}
+
+// Obtener número el promedio
+
+function numeroPromedio(array) {
+    let valorSuma = 0;
+    for (let i = 0; i < array.length; i++) {
+        valorSuma += array[i];
+    }
+    const promedio = valorSuma / array.length;
+}
+
+// Obtener el número más pequeño
+
+function numeroMasChico(array) {
+    let valorMenor = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (valorMenor >= array[i]) {
+            valorMenor = array[i];
+        }
+    }
+}
+
+// Obtener el número más grande
+
+function numeroMasGrande(array) {
+    let valorMayor = array[0];
+    for (let i = 0; i < array.length; i++) {
+        if (valorMayor <= array[i]) {
+            valorMayor = array[i];
+        }
+    }
+}
+
+// Obtener el número que más se repite
+
+function numeroQueMasRepite(array) {
+    let numeroQueRepite = 0;
+    let contador = 0;
+    let cuenta = 0;
+
+    array.map((p) => {
+        cuenta = 0;
+        array.map((x) => {
+            if (p == x) {
+                cuenta++;
+            }
+        });
+        if (cuenta > contador) {
+            contador = cuenta;
+            numeroQueRepite = p;
+        }
+    });
+
+    console.log(numeroQueRepite);
+}
