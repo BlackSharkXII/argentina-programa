8cdba2035ae92036ff15d12f1294b1a2a8b4970d
BlackSharkXII <perdomobraian@gmail.com>
2022-09-14 22:39:46 -0300

    Actualizo boton-restablecer.js
    Actualizo el boton para que ahora al restablecer y se cargue una nueva cantidad de familiares, no se muestre el resultado anterior de la cuentas.

--- clase-8/Ejercicio-2/js/boton-restablecer.js
+++ clase-8/Ejercicio-2/js/boton-restablecer.js
@@ -4,4 +4,13 @@ $botonRestablecer.onclick = function () {
     eliminarLabel();
     $divSeccionPregunta.className = '';
     $divSeccionEdades.className = 'oculto';
+
+    $labelResultadoMayor = document.querySelector('#resultado-mayor');
+    $labelResultadoMayor.innerText = '';
+
+    $labelResultadoMenor = document.querySelector('#resultado-menor');
+    $labelResultadoMenor.innerText = '';
+
+    $labelResultadoPromedio = document.querySelector('#resultado-promedio');
+    $labelResultadoPromedio.innerText = '';
 };
